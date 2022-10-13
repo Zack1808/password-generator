478c01a440d4050f21c87a176a001a27f7c5d37e
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-10-13 21:34:19 +0200

    Created a password generator funciton

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 // Importing the style file
 import '../css/Form.css';
 
+// Importing helper functions
+import { generatePassowrd } from '../utils/form.utils';
+
 // Creating the Form component
 const Form = () => {
+
+  const numberRef = useRef();
+  const symbolRef = useRef();
+  const lengthRef = useRef();
+
+  const [password, setPassword] = useState('')
+
+  // Function that will handle the submition of the from and generate a random password
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setPassword(generatePassowrd(numberRef.current.checked, symbolRef.current.checked, lengthRef.current.value || 6))
+  }
+  
+
   return (
-    <form className='password-form'>
+    <form className='password-form' onSubmit={onSubmit}>
       <h2>Generate a secure password!</h2>
       <div className='passowrd-inputs'>
-        <h4 className='password-text'>fadsfdas</h4>
+        <h4 className='password-text'>{password}</h4>
         <div className="flex">
           <label htmlFor="password-length">Password Length</label>
-          <input type="number" max={72} min={6} name="password-length" style={{ maxWidth: "8ch"}} />
+          <input type="number" max={72} min={6} name="password-length" style={{ maxWidth: "8ch"}} ref={lengthRef} />
         </div>
         <div className="flex">
           <label htmlFor="numbers">Include Numbers</label>
-          <input type="checkbox" name="numbers" />
+          <input type="checkbox" name="numbers" ref={numberRef} />
         </div>
         <div className="flex">
           <label htmlFor="symbols">Include Symbols</label>
-          <input type="checkbox" name="symbols" />
+          <input type="checkbox" name="symbols" ref={symbolRef} />
         </div>
         <button className="btn">Generate</button>
       </div>
---
+++ src/utils/form.utils.js
@@ -0,0 +1,37 @@
+// Function that will return a random letter
+const randomLetter = () => {
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let randomIndex = Math.floor(Math.random() * charset.length)
+    return charset[randomIndex]
+}
+
+// Function that will return a random number
+const randomNumber = () => {
+    let charset = "0123456789";
+    let randomIndex = Math.floor(Math.random() * charset.length)
+    return charset[randomIndex]
+}
+
+// Function that will return a random symbol
+const randomSymbol = () => {
+    let charset = "[]{}!?|-_*#/()&%$";
+    let randomIndex = Math.floor(Math.random() * charset.length)
+    return charset[randomIndex]
+}
+
+// Function that will generate the password
+export const generatePassowrd = (hasNubers = false, hasSymbols = false, len = 6) => {
+    let functionSet = [randomLetter];
+
+    functionSet = hasNubers ? [...functionSet, randomNumber] : functionSet
+    functionSet = hasSymbols ? [...functionSet, randomSymbol] : functionSet
+
+    let passowrd = [];
+    for(let i = 0; i < len; i++) {
+        let randomIndex = Math.floor(Math.random() * functionSet.length)
+        let character = functionSet[randomIndex]()
+        passowrd.push(character);
+    }
+
+    return passowrd.join("");
+}
